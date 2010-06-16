75687df46195a9a531cb92f5822162703f5eb4d7
Stephan_Lesch <devnull@localhost>
2010-06-16 17:13:56 +0000

    factored out the vendor and build tags

--- bin/_copy_dlls.bat
+++ bin/_copy_dlls.bat
@@ -1,40 +1,41 @@
 set WX_VENDOR=_muhkuh
+set WX_BUILD=
+rem @echo off
 
-@echo off
 echo copy wxwidgets dlls
-copy %WXWIN%\lib\vc_dll\wxbase28_net_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxbase28_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxbase28_xml_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_adv_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_aui_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_core_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_gl_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_html_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_media_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_qa_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_richtext_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_stc_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_vc%WX_VENDOR%.dll .
-copy %WXWIN%\lib\vc_dll\wxmsw28_xrc_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxbase28%WX_BUILD%_net_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxbase28%WX_BUILD%_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxbase28%WX_BUILD%_xml_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_adv_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_aui_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_core_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_gl_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_html_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_media_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_qa_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_richtext_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_stc_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_vc%WX_VENDOR%.dll .
+copy %WXWIN%\lib\vc_dll\wxmsw28%WX_BUILD%_xrc_vc%WX_VENDOR%.dll .
 
 echo copy wxlua dlls
 copy %WXLUA%\lib\vc_dll\lua5.1.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindadv.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindaui.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindbase.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindcore.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindgl.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindhtml.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindmedia.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindnet.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindstc.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindxml.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxbindxrc.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxlua.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxluadebug.dll .
-copy %WXLUA%\lib\vc_dll\wxlua_msw28_wxluasocket.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindadv.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindaui.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindbase.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindcore.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindgl.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindhtml.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindmedia.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindnet.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindstc.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindxml.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxbindxrc.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxlua.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxluadebug.dll .
+copy %WXLUA%\lib\vc_dll\wxlua_msw28%WX_BUILD%_wxluasocket.dll .
 
-echo copy system dlls
-copy %SystemRoot%\system32\msvcr71.dll .
-copy %SystemRoot%\system32\msvcp71.dll .
-
+@echo copy system dlls
+rem @copy %SystemRoot%\system32\msvcr71.dll .
+rem @copy %SystemRoot%\system32\msvcp71.dll .
+@xcopy /i /y "C:\Programme\Microsoft Visual Studio 8\VC\redist\x86\Microsoft.VC80.CRT" .\Microsoft.VC80.CRT
